9f32c5adee9c190b7c06ac14d34581525ccb0414
André das Trevas <atrevas@gmail.com>
2015-05-17 17:33:02 -0300

    Add a tabsetPanel to the main panel

--- server.R
+++ server.R
@@ -1,15 +1,8 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 library(ggplot2)
 library(scales)
 
-
+# Define the server logic for the shiny app
 shinyServer(function(input, output) {
 
   # Reactive function to update variable name
--- ui.R
+++ ui.R
@@ -1,5 +1,5 @@
 library(shiny)
-library(ggplot2)  # For the diamonds data set
+library(ggplot2)  # For the diamonds data set and plot functions
 
 # Get a list of the numeric variables from the diamonds data set
 l <- sapply(diamonds, function(x)  is.numeric(x))
@@ -8,23 +8,26 @@ var_names <- names(l[l == TRUE])
 shinyUI(
   fluidPage(
 
-  # Application title
-  titlePanel('Diamonds Variables')
+    # Application title
+    titlePanel('Diamonds Variables')
 
-  , sidebarLayout(
-    sidebarPanel(
-      helpText('Diamonds is a dataset containing the prices and other attributes of almost 54,000 diamonds.')
-      , selectInput('var', 'Select the variable to plot:', var_names)
-      , selectInput('color', 'Select the color:'
+    , sidebarLayout(
+       sidebarPanel(
+       helpText('The diamonds dataset contains the prices and other attributes of almost 54,000 diamonds.')
+       , helpText('This Shiny App displays a kernel density plot for one of the numeric variables of the diamonds data set.')
+       , selectInput('var', 'Select the variable to plot:', var_names)
+       , selectInput('color', 'Select the color:'
                     , choices = c('blue', 'green', 'darkgreen'))
-    ),
-
+       )
+    
     # Show a plot of the generated distribution
-    mainPanel(
-      h3('Kernel density plot', align = 'center')
-      , plotOutput("distPlot")
-      , h3('Summary of the variable', align = 'center')
-      , verbatimTextOutput('summaryText')
-    )
+    , mainPanel(
+       tabsetPanel(
+         type = 'tabs'  
+         , tabPanel('Plot', plotOutput("distPlot"))
+         , tabPanel('Summary', verbatimTextOutput('summaryText'))
+       ) 
+     )
+   )
   )
-))
+)
