cfe7674ed7e304bd8eb629c86059eb467b779167
André das Trevas <atrevas@gmail.com>
2015-05-19 20:45:48 -0300

    Add color to the box plot

--- server.R
+++ server.R
@@ -22,9 +22,9 @@ shinyServer(function(input, output) {
     # It is necessary to use aes_string to manage variable names in the call
     # to ggplot
     ggplot(diamonds, aes_string(x = vnum(),  fill = vcat())) +
-      geom_density(alpha = 0.3) +
+      geom_density(alpha = 0.7) +
       #geom_line(stat = 'density',  aes_string(colour = color())) +
-      scale_y_continuous(labels = percent) +
+      #scale_y_continuous(labels = percent) +
       ggtitle(title) +
       theme(plot.title = element_text(size = rel(2))) +
       xlab(x_label) +
@@ -37,8 +37,9 @@ shinyServer(function(input, output) {
     y_label <- paste0(toupper(substring(vnum(), 1, 1)), substring(vnum(), 2))
     title <- paste0('Box Plot - ', x_label, ' x ', y_label, '\n')
     
-    ggplot(diamonds, aes_string(x = vcat(), y = vnum())) +
+    ggplot(diamonds, aes_string(x = vcat(), y = vnum(), fill = vcat())) +
       geom_boxplot() +
+      guides(fill=FALSE) +
       ggtitle(title) +
       theme(plot.title = element_text(size = rel(2))) +
       xlab(x_label)  +
